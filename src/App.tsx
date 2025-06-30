0054486b4e53d1ad7698bce002ff086e370e4e5d
Rohit Omar <rohitomar@Rohits-MacBook-Air.local>
2025-06-30 14:03:10 +0530

    enable ts with vite and shadcn components

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <p className='p-10 m-10 font-medium text-lg'>
+      API Communicator
+      <Button className='m-4' variant='outline'>
+        FETCH
+      </Button>
+    </p>
+  );
+}
+
+export default App;
